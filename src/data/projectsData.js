276673ab6f9491493c2e0bca27588de3ad4b7485
bhushankhopkarr <bhushankhopkar06@gmail.com>
2024-10-03 21:43:36 +0530

    Add projectsData.js file with project data

---
+++ src/data/projectsData.js
@@ -0,0 +1,17 @@
+// src/data/projectsData.js
+const projectsData = [
+    {
+      title: 'Project One',
+      description: 'This is the description for project one.',
+      link: 'https://example.com/project-one',
+    },
+    {
+      title: 'Project Two',
+      description: 'This is the description for project two.',
+      link: 'https://example.com/project-two',
+    },
+    // Add more projects as needed
+  ];
+  
+  export default projectsData;
+  
